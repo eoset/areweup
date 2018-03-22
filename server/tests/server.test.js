5df77c5b69b8d468dc14d3bd90d07ea338f6435e
Erik Sällström <sallstrom@gmail.com>
2018-03-22 10:40:23 +0100

    Added several tests for exposed endpoints

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -6,6 +6,7 @@ const {app} = require('./../server');
 const {Incident} = require('./../models/incident');
 
 const incidents = [{
+    _id: new ObjectID(),
     description: "A test incident",
     status: "Ongoing",
     impact: "Disruption",
@@ -36,6 +37,43 @@ const incidents = [{
             tag: "monetary value"
         }
     ]
+},
+{
+    _id: new ObjectID(),
+    description: "Another test incident",
+    status: "Resolved",
+    impact: "Major outage",
+    responsibleService: "Lasse Kongo",
+    affectedServices: [
+        {
+            serviceName: "Bussen"
+        },
+    ],
+    reporter: "erik.sallstrom@visma.com",
+    createdAt: new Date().getTime(),
+    resolvedAt: null,
+    totalResolutionTime: null,
+    incidentWorkflow: [
+        {
+            body: "Incident detected, work initiated",
+            createdAt: new Date().getTime()
+        },
+        {
+            body: "Found issue, developers resolved it",
+            createdAt: new Date().getTime()
+        }
+    ],
+    tags: [
+        {
+            tag: "azure"
+        },
+        {
+            tag: "database"
+        },
+        {
+            tag: "io"
+        }
+    ]
 }];
 
 beforeEach((done) => {
@@ -48,11 +86,73 @@ beforeEach((done) => {
     }).then(() => done());
 });
 
+// GET /incidents
 describe('GET /incidents', () => {
-    it('should return a incident', (done) => {
+    it('should return test incidents and assert on length', (done) => {
+        var description = "A test incident";
         request(app)
             .get('/incidents')
             .expect(200)
-            .end(done);
+            .expect((res) => {
+                expect(res.body.incidents.length).toBe(2);
+            })
+            .end((err, res) => {
+                if(err) {
+                    return done(err);
+                }
+                else {
+                    return done();
+                }
+            })
+            
     }).timeout(5000);
+});
+
+// GET /incidents/:id
+describe('GET /incidents/:id', () => {
+    it('should return incident doc', (done) => {
+        var hexId = incidents[0]._id.toHexString();
+        request(app)
+            .get(`/incidents/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.incident._id).toBe(hexId);
+            })
+            .end(done);
+    });
+
+    it('should return 404 if not found', (done) => {
+        request(app)
+            .get(`/incidents/${new ObjectID().toHexString()}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 400 for invalid ObjectIDs', (done) => {
+        request(app)
+            .get(`/incidents/abc123`)
+            .expect(400)
+            .end(done);
+    });
+});
+
+
+// POST /incidents
+describe('POST /incidents', () => {
+    it('should not create incident with invalid body data', (done) => {
+        request(app)
+            .post('/incidents')
+            .send({})
+            .expect(400)
+            .end((err, res) => {
+                if(err) {
+                    return done(err);
+                }
+            });
+
+            Incident.find().then((incidents) => {
+                expect(incidents.length).toBe(2);
+                done();
+            }).catch((e) => done(e));
+    })
 });
