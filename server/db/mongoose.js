0a8d2647ec18be2ee5ad2224651048d6dd255a59
Erik Sällström <sallstrom@gmail.com>
2018-03-23 17:07:23 +0100

    Removed connectionstring

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/AreWeUp');
-//mongoose.connect('mongodb://applicationUser:bull69fitta@46.101.242.67:27017/AreWeUp?authSource=admin');
 
 
 module.exports = {mongoose};
